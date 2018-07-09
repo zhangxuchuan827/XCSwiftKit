da943e89bc749438b9bb3b7271efa33d1e9373ee
zhangxuchuan <zhangxuchuan@zhangs-Mac.local>
2018-07-09 19:18:54 +0800

    更新

---
+++ Classes/Base/UIKit/XCUIColorExtension.swift
@@ -0,0 +1,78 @@
+//
+//  XCUIColorExtension.swift
+//  XCSwiftKitDemo
+//
+//  Created by zhangxuchuan on 2018/7/9.
+//  Copyright © 2018年 zhangxuchuan. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func hex(hex: String) -> UIColor {
+        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        if cString.count < 6 { return UIColor.black }
+        
+        let index = cString.index(cString.endIndex, offsetBy: -6)
+        let subString = cString[index...]
+        if cString.hasPrefix("0X") { cString = String(subString) }
+        if cString.hasPrefix("0x") { cString = String(subString) }
+        if cString.hasPrefix("#") { cString = String(subString) }
+        
+        if cString.count != 6 { return UIColor.black }
+        
+        var range: NSRange = NSMakeRange(0, 2)
+        let rString = (cString as NSString).substring(with: range)
+        range.location = 2
+        let gString = (cString as NSString).substring(with: range)
+        range.location = 4
+        let bString = (cString as NSString).substring(with: range)
+        
+        var r: UInt32 = 0x0
+        var g: UInt32 = 0x0
+        var b: UInt32 = 0x0
+        
+        Scanner(string: rString).scanHexInt32(&r)
+        Scanner(string: gString).scanHexInt32(&g)
+        Scanner(string: bString).scanHexInt32(&b)
+        
+        return UIColor(r: r, g: g, b: b)
+    }
+    
+    
+    /// 输入RGBA格式的数据， 不需要除以255.0
+    convenience init(r:UInt32 ,g:UInt32 , b:UInt32 , a:CGFloat = 1.0) {
+        self.init(red: CGFloat(r) / 255.0,
+                  green: CGFloat(g) / 255.0,
+                  blue: CGFloat(b) / 255.0,
+                  alpha: a)
+    }
+    
+    ///将颜色转换成hex色值
+    func hexStirng() -> String {
+        
+        
+        
+        return ""
+    }
+    
+    /// 生成一个随机颜色
+    class var random: UIColor {
+        return UIColor(r: arc4random_uniform(256),
+                       g: arc4random_uniform(256),
+                       b: arc4random_uniform(256))
+    }
+    
+    /// 生成一个纯色快的图片对象
+    func image() -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context!.setFillColor(self.cgColor)
+        context!.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
